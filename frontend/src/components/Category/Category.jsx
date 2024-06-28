e9c8148d95d026a7d180964b24c13a7e05e6eeb6
Maitri0607 <milipatel555@gmail.com>
2024-06-28 13:48:44 +0530

    category and services

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -22,7 +22,7 @@ const Category = () => {
                                     textColor={"text-white"} />
                             </div>
                         </div>
-                        <img src={image1} alt=""  className="w-[320px] absolute bottom-0"/>
+                        <img src={image1} alt="" className="w-[320px] absolute bottom-0" />
                     </div>
                     {/* Second col */}
                     <div className="py-10 pl-5 bg-gradient-to-br from-brandYellow to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end">
@@ -37,9 +37,25 @@ const Category = () => {
                                     textColor={"text-brandYellow"} />
                             </div>
                         </div>
-                        <img src={image2} alt=""  className="w-[320px] absolute -right-4 lg:top-[40px]"/>
+                        <img src={image2} alt="" className="w-[320px] absolute -right-4 lg:top-[40px]" />
                     </div>
                     {/* third col */}
+                    <div className="col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end">
+                        <div>
+                            <div className=' mb-4'>
+                                <p className='mb-[2px] text-white'>Enjoy</p>
+                                <p className='text-2xl font-semibold mb-[2px]'>With</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Laptop</p>
+                                <Button
+                                    text="Browse"
+                                    bgColor={"bg-white"} mb
+                                    textColor={"text-primary"} />
+                            </div>
+                        </div>
+                        <img src={image3}
+                            alt=""
+                            className="w-[250px] absolute top-1/2 -translate-y-1/2 -right-0" />
+                    </div>
                 </div>
             </div>
         </div>
---
+++ frontend/src/components/Services/Services.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { FaCarSide, FaHeadphonesAlt, FaWallet, FaCheckCircle } from "react-icons/fa";
+
+const ServiceData = [
+  {
+    id: 1,
+    icon: <FaCarSide className='text-4xlx md:text-5xl text-primary' />,
+    title: "Free Shipping",
+    descriiption: "Free Shipping On All Order",
+  },
+  {
+    id: 2,
+    icon: < FaCheckCircle className='text-4xlx md:text-5xl text-primary' />,
+    title: "Safe Money",
+    descriiption: "30 Days Money Back",
+  },
+  {
+    id: 3,
+    icon: <FaWallet className='text-4xlx md:text-5xl text-primary' />,
+    title: "Secure Payment",
+    descriiption: "All Payment Secure",
+  },
+  {
+    id: 4,
+    icon: <FaHeadphonesAlt className='text-4xlx md:text-5xl text-primary' />,
+    title: "Online Support 24/7",
+    descriiption: "Technical Support 24/7",
+  },
+];
+
+const Services = () => {
+  return (
+    <>
+      <div className='container mt-14 md:mt-20'>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-4 ">
+          {
+            ServiceData.map((data) => (
+              <div className='flex flex-col items-start sm:flex-row gap-4'>
+                {data.icon}
+                <div>
+                  <h1 className='lg:text-xl font-bold'>{data.title}</h1>
+                  <h1 className='text-sm text-gray-400'>{data.descriiption}</h1>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Services
